12539125c645279ead272cb5e6f0bd00be8d9acf
Umair Hamza <imumairhamza@gmail.com>
2023-11-02 20:06:11 +0500

    Create Controllers

---
+++ app/controllers/api/v1/commissioned_bies_controller.rb
@@ -0,0 +1,2 @@
+class Api::V1::CommissionedBiesController < ApplicationController
+end
---
+++ test/controllers/api/v1/monthly_services_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Api::V1::MonthlyServicesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
